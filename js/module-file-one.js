44c24d8b2a86d6ff876cc703289873eca4540493
ari_fakhri <arifakhri97@gmail.com>
2016-10-26 07:48:10 +0700

    initial commit

---
+++ js/module-file-one.js
@@ -0,0 +1,7 @@
+var moduleCtrl = angular.module('modLocalStorage', ['ngStorage']);
+moduleCtrl.controller('ctrlLocalStr', function($scope, $localStorage) {
+    $scope.storage = $localStorage.$default({
+       username: "Dwikuntobayu",
+        age: 17
+    });
+});
